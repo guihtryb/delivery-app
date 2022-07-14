305bc33667c28071a0a647646f29d9d5b2795323
Guilherme Viana <guilhermehviana01@gmail.com>
2022-07-14 17:28:48 -0300

    fix: table' name to lowercase

--- back-end/src/database/models/product.js
+++ back-end/src/database/models/product.js
@@ -1,5 +1,5 @@
 const Product = (sequelize, DataTypes) => {
-  const Product = sequelize.define("Product", {
+  const Product = sequelize.define("product", {
     name: DataTypes.STRING,
     price: DataTypes.NUMBER,
     url_image: DataTypes.STRING
