0f733d4d3689f1dcf42f2bd7a68749d5505eec43
Ivan Mastrangelo Dias <masttrangello@gmail.com>
2022-07-15 17:32:16 -0300

    feat: terminar a userService usando o passwordEncryptor

--- back-end/src/services/usersService.js
+++ back-end/src/services/usersService.js
@@ -1,40 +1,57 @@
-const { user: User } = require('../database/models/user');
 const userVerify = require('./validates/userVerify');
+const { user } = require('../database/models');
 const conflict = require('../errors/conflicts');
 // const userExist = require('./validates/userExist');
 const notFound = require('../errors/notFound');
 const passwordEncryptor = require('../helpers/passwordEncryptor');
+const { unauthorized } = require('../errors/conflicts');
 
-const create = async ({ name, email, password, role }) => {
+const create = async ({ name, email, password }) => {
   const encryptedPassword = passwordEncryptor(password);
   
   const isUserRegistered = await userVerify(email, encryptedPassword);
 
   if (isUserRegistered) throw conflict('User already registered');
 
-  const user = await User.create({ name, email, encryptedPassword, role });
+  const createUser = await user.create({ name, email, encryptedPassword });  
          
-  return user;
+  return createUser;
 };
 
 const getAllUsers = async () => {
-  const users = await User.findAll({ attributes: { exclude: 'password' } });
+  const users = await user.findAll({ attributes: { exclude: 'password' } });
   return users;
 };
 
 const getUserById = async (id) => {
-  const user = await User.findByPk(id, { attributes: { exclude: 'password' } });
-  if (!user) throw notFound('User does not exist');
-  return user; 
+  const getUser = await user.findByPk(id, { attributes: { exclude: 'password' } });
+  if (!getUser) throw notFound('User does not exist');
+  return getUser; 
+};
+
+const update = async (body, id) => {
+  const { name, email, password, role } = body;
+  const checkPassword = await user.findByPk(id);
+  const encryptedPassword = passwordEncryptor(password);
+  if (checkPassword.id !== encryptedPassword || role !== 'admin') {
+    throw unauthorized('Unauthorized user');
+  }
+  if (role === 'admin') {
+    const userUpdated = await user.update({ name, email, role }, { where: { id } });
+    return userUpdated;
+  }
+  const userUpdated = await user.update({ name, email }, { where: { id } });
+    return userUpdated;
 };
 
 const destroy = async (id) => {
-  await User.destroy({ where: { id } });
+  await user.destroy({ where: { id } });
 };
 
 module.exports = {
   create,
   getAllUsers,
   getUserById,
+  update,
   destroy,
 };
