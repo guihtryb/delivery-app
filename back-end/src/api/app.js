6199467c57ee27787193ee3ffe2a2a2a35c5b3b9
Guilherme Viana <guilhermehviana01@gmail.com>
2022-07-15 18:32:04 -0300

    feat(routes): add products routes

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,6 +1,12 @@
+const { json } = require('body-parser');
 const express = require('express');
+const productsRouter = require('../routes/productsRouter');
 
 const app = express();
+
+app.use(json());
+
+app.use(productsRouter);
 
 app.get('/coffee', (_req, res) => res.status(418).end());
 
