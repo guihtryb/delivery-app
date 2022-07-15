0b093ef3c6ef362a15dbd1d60ae28c5a96c96d9d
Ivan Mastrangelo Dias <masttrangello@gmail.com>
2022-07-15 19:53:19 -0300

    fix: correção da importação do cors

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const { json } = require('body-parser');
-const { cors } = require('cors');
+const cors = require('cors');
 const usersRoutes = require('../routes/usersRoutes');
 const errors = require('../middlewares/errors');
 
 const app = express();
 app.use(json());
-app.use(cors);
+app.use(cors());
 
 app.get('/coffee', (_req, res) => res.status(418).end());
 app.use(usersRoutes);
