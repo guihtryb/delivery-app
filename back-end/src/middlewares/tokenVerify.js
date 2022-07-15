01fcb173fba6cdbf973416f3c2a3ea105bfd0ad7
Ivan Mastrangelo Dias <masttrangello@gmail.com>
2022-07-15 17:18:38 -0300

    feat: criar pasta middlewares com os middlewares errors, tokenVerify e userDataVerify

---
+++ back-end/src/middlewares/tokenVerify.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+
+const tokenVerify = (req, res, next) => {
+  try {
+    const token = req.headers.authorization;
+
+    if (!token) {
+      return res.status(401).json({ message: 'Token not found' });
+    }
+    const secretKey = 'só$abeQu&mviu';
+    const payload = jwt.verify(token, secretKey);
+    req.user = payload.id;
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: 'Expired or invalid token' });
+  }
+  };
+
+module.exports = tokenVerify; 
---
+++ back-end/src/middlewares/userDataVerify.js
@@ -0,0 +1,22 @@
+const Joi = require('joi');
+const badRequest = require('../errors/badRequest');
+
+const userSchema = Joi.object({
+  name: Joi.string().min(8).required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().length(6).required(),
+});
+
+const userDataVerify = (req, res, next) => {
+  try {
+    const { name, email, password } = req.body;
+    const { error } = userSchema.validate({ name, email, password });
+    if (error) throw badRequest(error.message);
+
+  next();
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = userDataVerify; 
