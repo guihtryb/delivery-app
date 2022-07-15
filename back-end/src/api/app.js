c5aa7c0c38c3b5a66faccf28bff0137bb952ffdc
Guilherme Viana <guilhermehviana01@gmail.com>
2022-07-15 19:34:18 -0300

    app updated

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,5 +1,5 @@
 const { json } = require('body-parser');
-const cors = require('cors');
+const { cors } = require('cors');
 const express = require('express');
 const productsRouter = require('../routes/productsRouter');
 const usersRoutes = require('../routes/usersRoutes');
@@ -9,11 +9,11 @@ const app = express();
 app.use(json());
 app.use(cors);
 
-app.use(errors);
-
-app.use(productsRouter);
 app.use(usersRoutes);
+app.use(productsRouter);
 
 app.get('/coffee', (_req, res) => res.status(418).end());
+
+app.use(errors);
 
 module.exports = app;
