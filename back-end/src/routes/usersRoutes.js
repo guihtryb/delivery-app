822d4e4952eeace790ec4e63415317d7c927f72b
Ivan Mastrangelo Dias <masttrangello@gmail.com>
2022-07-15 17:35:36 -0300

    feat: configurar as rotas em route/usersRoutes

---
+++ back-end/src/routes/usersRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const router = express.Router();
+const userController = require('../controllers/usersController');
+const userDataVerify = require('../middlewares/userDataVerify');
+
+router.post('/users', userDataVerify, userController.create);
+router.get('/users', userController.getAllUsers);
+router.get('/users/:id', userController.getUserById);
+router.put('/users/:id', userController.update);
+router.delete('/users', userController.destroy);
+
+module.exports = router;
