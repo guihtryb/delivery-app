46f81ab10452bb42d69e6bab06f6665ff8554f7b
João Victor <joao.lenny100@gmail.com>
2022-07-16 16:02:18 -0300

    Controller de sales
    - Criando arquivo salesController.js;
    - Criando função createSale;
    - Criando função deleteSale;
    - Criando função getAllSales;
    - Criando função getSaleById;
    - Criando função updateSale.

---
+++ back-end/src/controllers/salesController.js
@@ -0,0 +1,52 @@
+const salesService = require('../services/salesService');
+
+const createSale = async (req, res, next) => {
+  try {
+    const { body, params: { id } } = req;
+    const newSale = await salesService.createSale(id, body);
+    return res.status(201).json(newSale);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const deleteSale = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    await salesService.deleteSale(id);
+    return res.status(204).end();
+  } catch (error) {
+    next(error);
+  }
+};
+
+const getAllSales = async (_req, res, next) => {
+  try {
+    const sales = await salesService.getAllSales();
+    return res.status(200).json(sales);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const getSaleById = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const sale = await salesService.getSaleById(id);
+    return res.status(200).json(sale);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const updateSale = async (req, res, next) => {
+  try {
+    const { body, params: { id } } = req;
+    const updatedSale = await salesService.updateSale(id, body);
+    return res.status(201).json(updatedSale);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export { createSale, deleteSale, getAllSales, getSaleById, updateSale };
