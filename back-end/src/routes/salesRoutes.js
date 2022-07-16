284628bec76395b0ff8858dc25ddb32aff70dfb6
João Victor <joao.lenny100@gmail.com>
2022-07-16 16:22:08 -0300

    Routes de sales
    - Criando arquivo salesRoutes.js;
    - Criando rota do tipo post para createSale;
    - Criando rota do tipo delete para deleteSale;
    - Criando rota do tipo get para getAllSales;
    - Criando rota do tipo get para getSaleById;
    - Criando rota do tipo put para updateSale;

---
+++ back-end/src/routes/salesRoutes.js
@@ -0,0 +1,13 @@
+const { Router } = require('express');
+const salesController = require('../controllers/salesController');
+
+const router = Router();
+const { createSale, deleteSale, getAllSales, getSaleById, updateSale } = salesController;
+
+router.post('/sales', createSale);
+router.delete('/sales', deleteSale);
+router.get('/sales', getAllSales);
+router.get('/sales/:id', getSaleById);
+router.put('/sales/:id', updateSale);
+
+module.exports = router;
