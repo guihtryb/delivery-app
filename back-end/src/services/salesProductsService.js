1b29bc04337100d712c2cbf934be23b7fe53a17b
João Victor <joao.lenny100@gmail.com>
2022-07-16 13:29:31 -0300

    Service de salesProducts
    - Removendo arquivo salesProductsService.js.

--- back-end/src/services/salesProductsService.js
+++
@@ -1,5 +0,0 @@
-const { saleProduct: saleProductModel } = require('../database/models/saleProduct');
-
-const getAllById = async (saleId) => saleProductModel.findAll({ where: { saleId } });
-
-export default getAllById;
