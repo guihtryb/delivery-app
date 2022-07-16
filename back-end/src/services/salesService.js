345a076175368e3ef4c16fcd29d503a9a5409f44
João Victor <joao.lenny100@gmail.com>
2022-07-15 21:14:07 -0300

    Service de sales
    - Criando função formingProducts;
    - Criando função calcTotalPrice;
    - Adicionando mais informações na função getAllSales.

--- back-end/src/services/salesService.js
+++ back-end/src/services/salesService.js
@@ -1,10 +1,51 @@
-const { sale: saleModel } = require('../database/models');
+const { getProductById } = require('./productsService');
+const { getAllById } = require('./salesProductsService');
+const { sale: saleModel, user: userModel } = require('../database/models');
 
 const createSale = async (id, newSale) => saleModel.create(id, newSale);
 
 const deleteSale = async (id) => saleModel.delete(id);
 
-const getAllSales = async () => saleModel.findAll();
+const formingProducts = async (saleId) => {
+  const salesProducts = await getAllById(saleId);
+
+  return salesProducts.map(async (saleProduct) => {
+    const { productId, quantity } = saleProduct;
+    const product = await getProductById(productId);
+    const { id, name, price, urlImage } = product;
+
+    return { id, name, price, urlImage, quantity };
+  });
+};
+
+const calcTotalPrice = async (products) => {
+  let totalPrice = 0;
+
+  products.forEach((product) => {
+    totalPrice += product.price * product.quantity;
+  });
+
+  return totalPrice;
+};
+
+const getAllSales = async () => {
+  const sales = await saleModel.findAll({
+    include: [
+      { attributes: ['userId'], model: userModel },
+      { attributes: ['sellerId'], model: userModel },
+    ],
+  });
+
+  return sales.map(async (sale) => {
+    const { id, userId, sellerId, deliveryAddress, deliveryNumber, saleDate, status } = sale;
+    const products = await formingProducts(id);
+    const totalPrice = await calcTotalPrice(products);
+
+    return {
+      id, userId, sellerId, totalPrice, deliveryAddress, deliveryNumber, saleDate, status, products,
+    };
+  });
+};
 
 const getSaleById = async (id) => saleModel.findOne(id);
 
