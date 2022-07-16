1932f3bae3547473d7d0f67d40f71861c5844884
João Victor <joao.lenny100@gmail.com>
2022-07-16 16:23:35 -0300

    App
    - Utilizando rotas de sales;
    - Ajustando a ordem dos "require's".

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,16 +1,19 @@
-const { json } = require('body-parser');
 const { cors } = require('cors');
 const express = require('express');
-const productsRouter = require('../routes/productsRouter');
-const usersRoutes = require('../routes/usersRoutes');
+const { json } = require('body-parser');
 const errors = require('../middlewares/errors');
+const salesRoutes = require('../routes/salesRoutes');
+const usersRoutes = require('../routes/usersRoutes');
+const productsRouter = require('../routes/productsRouter');
 
 const app = express();
+
 app.use(json());
 app.use(cors);
 
 app.use(usersRoutes);
 app.use(productsRouter);
+app.use(salesRoutes);
 
 app.get('/coffee', (_req, res) => res.status(418).end());
 
