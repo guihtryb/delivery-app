3aa4539e6eb2bbe9b3faacedc121160cc3c3c8ac
Ivan Mastrangelo Dias <masttrangello@gmail.com>
2022-07-16 12:23:45 -0300

    feat: inserir a rota de login no App

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -4,12 +4,15 @@ const express = require('express');
 const productsRouter = require('../routes/productsRouter');
 const usersRoutes = require('../routes/usersRoutes');
 const registerRoutes = require('../routes/registerRoutes');
+const loginRoutes = require('../routes/loginRoutes');
 const errors = require('../middlewares/errors');
 
 const app = express();
 app.use(json());
 app.use(cors());
 
+app.use(registerRoutes);
+app.use(loginRoutes);
 app.use(usersRoutes);
 app.use(productsRouter);
 app.use(registerRoutes);
