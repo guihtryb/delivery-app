31fdcf018aaaf9aeb0cb24a54b6ac959c29d0b4f
João Victor <joao.lenny100@gmail.com>
2022-07-20 19:08:57 -0300

    Routes de sales
    - Editando rota do tipo post para createSale;
    - Criand rota do tipo get para getAllSalesByUser.

--- back-end/src/routes/salesRoutes.js
+++ back-end/src/routes/salesRoutes.js
@@ -1,12 +1,21 @@
 const { Router } = require('express');
+const tokenVerify = require('../middlewares/tokenVerify');
 const salesController = require('../controllers/salesController');
 
 const router = Router();
-const { createSale, deleteSale, getAllSales, getSaleById, updateSale } = salesController;
+const { 
+  createSale,
+  deleteSale,
+  getAllSales,
+  getAllSalesByUser,
+  getSaleById,
+  updateSale,
+} = salesController;
 
-router.post('/sales/:userId', createSale);
+router.post('/sales', tokenVerify, createSale);
 router.delete('/sales', deleteSale);
 router.get('/sales', getAllSales);
+router.get('/sales/seller', tokenVerify, getAllSalesByUser);
 router.get('/sales/:id', getSaleById);
 router.put('/sales/:id', updateSale);
 
