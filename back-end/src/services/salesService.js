d3a870630cf952bbf1a00509921b6c795153973d
João Victor <joao.lenny100@gmail.com>
2022-07-20 19:02:59 -0300

    Service de sales
    - Atualizando função createSale;
    - Atualizando função updateSale;
    - Criando função getAllSalesByUser.

--- back-end/src/services/salesService.js
+++ back-end/src/services/salesService.js
@@ -1,15 +1,21 @@
 const { sale: saleModel, user: userModel } = require('../database/models');
 
 const createSale = async (userId, saleInfos) => {
-  const { totalPrice, deliveryAddress, deliveryNumber, status } = saleInfos;
+  const { totalPrice, sellerName, deliveryAddress, deliveryNumber, status } = saleInfos;
+  const { id: sellerId } = await userModel.findOne({ where: { name: sellerName } });
   const saleDate = new Date();
 
-  const params = { totalPrice, userId, deliveryAddress, deliveryNumber, saleDate, status };
-  const { id } = await saleModel.create(params, { where: { userId } });
+  const params = {
+    totalPrice, userId, sellerId, deliveryAddress, deliveryNumber, saleDate, status,
+  };
+
+  const { id } = await saleModel.create(params);
+
   const newSale = {
     id,
     totalPrice,
     userId,
+    sellerId,
     deliveryAddress,
     deliveryNumber,
     saleDate: JSON.stringify(saleDate).split('T')[0].slice(1),
@@ -25,14 +31,19 @@ const getAllSales = async () => saleModel.findAll({
   include: { as: 'users', model: userModel, attributes: ['id'] },
 });
 
+const getAllSalesByUser = async (id) => (
+  saleModel.findAll({ where: { sellerId: id } || { userId: id } })
+);
+
 const getSaleById = async (id) => saleModel.findOne(id);
 
-const updateSale = async (id, newSaleInfo) => saleModel.update(id, newSaleInfo);
+const updateSale = async (id, status) => (saleModel.update({ where: { id } }, { status }));
 
 module.exports = {
   createSale,
   deleteSale,
   getAllSales,
+  getAllSalesByUser,
   getSaleById,
   updateSale,
 };
