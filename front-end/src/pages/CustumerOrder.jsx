b39dea1578a3387361083a1cf39fcce5d33eaa23
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2022-07-22 12:25:40 -0300

    feat: modificando a CustomerOrder

--- front-end/src/pages/CustumerOrder.jsx
+++ front-end/src/pages/CustumerOrder.jsx
@@ -1,27 +1,59 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import axios from 'axios';
 import PropTypes from 'prop-types';
 import deliveryContext from '../context/deliveryContext';
 import DetailsOrder from '../components/DetailsOrder';
 import Navbar from '../components/Navbar';
 
-function CustomerOrder({ history }) {
-  const { ordersSelected } = useContext(deliveryContext);
+// function CustomerOrder({ history }) {
+function CustomerOrder() {
+  // const { ordersSelected } = useContext(deliveryContext);
+  const [order, setOrder] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
+  const { pathname } = useLocation();
 
   // obs: ESSA PAGINA SERA RENDERIZADA QUANDO A PESSOA USUARIA CONCLUIR UM PEDIDO NO CARRINHO, OU CLICAR EM UM PEDIDO
   // DA SUA LISTA DE PEDIDOS, ENTAO É PRECISO PASSAR PARA ESSA PAGINA O PEDIDO CORRETO A SER RENDERIZADO,
   // A LOGICA Q EU ESCOLHI AQUI, FOI SALVAR NO ESTADO GLOBAL O ULTIMO PEDIDO SELECIONADO, PARA ASSIM, QUANDO A GENTE ENTRAR
   // NA PAGINA DE DETAILS, SO RENDERIZAR ESSE PEDIDO SALVO GLOBALMENTE
 
-  console.log(ordersSelected, history);
+  // console.log(ordersSelected, history);
+
+  useEffect(() => {
+    // const { token } = JSON.parse(localStorage.getItem('user'));
+    const url = pathname.split('/');
+    const id = url[url.length - 1];
+    console.log(id);
+
+    axios
+      .get(`http://localhost:3001/sales/${id}`)
+      .then((response) => {
+        setOrder(response.data);
+        setIsLoading(false);
+        console.log(response.data);
+        console.log('aqui');
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
 
   return (
     <div className="order-page flex-column">
-      <Navbar />
-      <DetailsOrder
+      {/* <Navbar /> */}
+      { isLoading ? (<p>Loading</p>) : (
+        <DetailsOrder
+          name={ order.seller }
+          pedidos={ order }
+          data={ order.date }
+        />
+      )}
+      {/* <DetailsOrder
         name={ ordersSelected.seller }
         pedidos={ ordersSelected }
         data={ ordersSelected.date }
-      />
+      />   */}
     </div>
   );
 }
