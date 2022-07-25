3116fe8b0444189561e6319fb35c8637b596dd12
Guilherme Viana <guilhermehviana01@gmail.com>
2022-07-25 02:24:50 -0300

    feat(services/usersService): adiciona comentários para trabalho em progresso

--- back-end/src/services/usersService.js
+++ back-end/src/services/usersService.js
@@ -32,6 +32,7 @@ const getUserById = async (id) => {
   return getUser; 
 };
 
+// wip - sugestão de receber password e newPassword
 const update = async (body, id) => {
   const { name, email, password } = body;
   const checkPassword = await user.findByPk(id);
