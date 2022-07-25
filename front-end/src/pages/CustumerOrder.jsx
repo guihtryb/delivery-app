ec64b832e40be4fab68a197486b86f932211095c
Guilherme Viana <guilhermehviana01@gmail.com>
2022-07-25 02:14:52 -0300

    refactor(pages/CustomerOrder): refatora CustomerOrder faltando receber prop sale

--- front-end/src/pages/CustumerOrder.jsx
+++ front-end/src/pages/CustumerOrder.jsx
@@ -1,14 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
-import axios from 'axios';
 import PropTypes from 'prop-types';
-// import deliveryContext from '../context/deliveryContext';
 import DetailsOrder from '../components/DetailsOrder';
-// import Navbar from '../components/Navbar';
+// import deliveryContext from '../context/deliveryContext';
+import salesService from '../services/sales';
+import Navbar from '../components/Navbar';
 
-// function CustomerOrder({ history }) {
 function CustomerOrder() {
-  const { ordersSelected } = useContext(deliveryContext);
+  // const { ordersSelected } = useContext(deliveryContext);
   const [order, setOrder] = useState('');
   const [isLoading, setIsLoading] = useState(true);
   const { pathname } = useLocation();
@@ -18,31 +17,31 @@ function CustomerOrder() {
     const url = pathname.split('/');
     const id = url[url.length - 1];
 
-    axios.get(`http://localhost:3001/sales/${id}`)
-      .then((response) => {
-        setOrder(response.data);
-        setIsLoading(false);
-      })
-      .catch((err) => {
-        console.error(err);
-      });
+    const loadOrder = async () => {
+      const orderData = await salesService.getBySaleId(id);
+      setOrder(orderData);
+    };
+    loadOrder();
+
+    setIsLoading(false);
   }, [pathname]);
 
   return (
     <div className="order-page flex-column">
-      {/* <Navbar /> */}
+      <Navbar />
       { isLoading ? (<p>Loading</p>) : (
         <DetailsOrder
-          name={ order.seller }
-          pedidos={ order }
-          data={ order.date }
+          // wip - sale=order, e remover outras props
+          name="Fulana Pereira"
+          pedidos={ [order] } // wip - order.products
+          data={ order.saleDate }
         />
       )}
-      <DetailsOrder
+      {/* <DetailsOrder - não entendi,
         name={ ordersSelected.seller }
         pedidos={ ordersSelected }
         data={ ordersSelected.date }
-      />
+      /> */}
     </div>
   );
 }
