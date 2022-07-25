c8bac4d1f9c2fb351b55fb2896bef02be479a043
Guilherme Viana <guilhermehviana01@gmail.com>
2022-07-25 00:05:48 -0300

    feat(services/users): adiciona demais método para service de usuários

--- front-end/src/services/users.js
+++ front-end/src/services/users.js
@@ -1,18 +1,29 @@
-import { API_BASE, basicFetchRequisition } from '.';
+import {
+  API_BASE,
+  basicDeleteRequisition,
+  basicGetRequisition,
+  basicPostRequisition,
+  basicUpdateRequisition,
+  getRequisitionWithParams,
+} from '.';
 
 const filterByRole = (role, userData) => userData.filter((item) => item.role === role);
 
 const usersService = {
   getSellers: async () => [
-    ...filterByRole('seller', await basicFetchRequisition(API_BASE, 'users')),
+    ...await basicGetRequisition(API_BASE, 'users/sellers'),
   ],
   getAdmins: async () => [
-    ...filterByRole('administrator', await basicFetchRequisition(API_BASE, 'users')),
+    ...filterByRole('administrator', await basicGetRequisition(API_BASE, 'users')),
   ],
   getCustomers: async () => [
-    ...filterByRole('customer', await basicFetchRequisition(API_BASE, 'users')),
+    ...filterByRole('customer', await basicGetRequisition(API_BASE, 'users')),
   ],
-  getAll: async () => [...await basicFetchRequisition(API_BASE, 'users')],
+  getAll: async () => [...await basicGetRequisition(API_BASE, 'users')],
+  getUserById: async (id) => getRequisitionWithParams(API_BASE, 'users', id),
+  createUser: async (data) => basicPostRequisition(API_BASE, 'users', data),
+  updateUser: async (id, data) => basicUpdateRequisition(API_BASE, 'users', id, data),
+  deleteUser: async (id) => basicDeleteRequisition(API_BASE, 'users/me', id),
 };
 
 export default usersService;
