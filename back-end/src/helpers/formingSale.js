8b52769336868e357afc6b4414b6048111e51d31
João Victor <joao.lenny100@gmail.com>
2022-07-26 20:39:20 -0300

    Helpers
    - Removendo função formingProducts;
    - Refatorando função formingSale.

--- back-end/src/helpers/formingSale.js
+++ back-end/src/helpers/formingSale.js
@@ -1,12 +1,18 @@
 const formingSaleDate = require('./formingSaleDate');
+const { user: userModel } = require('../database/models');
 
-const formingSale = (id, saleParams, products) => {
-  const { totalPrice, userId, deliveryAddress, deliveryNumber, saleDate, status } = saleParams;
+const formingSale = async (id, saleParams, products) => {
+  const {
+    totalPrice, userId, sellerId, deliveryAddress, deliveryNumber, saleDate, status,
+  } = saleParams;
+
+  const { name: sellerName } = await userModel.findByPk(sellerId);
 
   return {
     id,
     totalPrice,
     userId,
+    sellerName,
     deliveryAddress,
     deliveryNumber,
     saleDate: formingSaleDate(saleDate),
